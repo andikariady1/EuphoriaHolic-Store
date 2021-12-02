7de39864d17ec437dd1a19a91b6800287cd64818
andikariady1 <andikariady714@gmail.com>
2021-12-02 14:25:31 +0700

    Project WP2 SMT3

---
+++ application/models/ModelKategori.php
@@ -0,0 +1,14 @@
+<?php
+
+class ModelKategori extends CI_Model
+{
+    public function data_pakaian_pria()
+    {
+        return $this->db->get_where("barang", array('kategori' => 'pakaian pria'));
+    }
+
+    public function data_pakaian_wanita()
+    {
+        return $this->db->get_where("barang", array('kategori' => 'pakaian wanita'));
+    }
+}
